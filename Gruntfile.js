d471edc62fc14dcafd620828a42e6b59a0194ff7
Murat <30314594+munalfd@users.noreply.github.com>
2020-08-10 00:56:44 +0300

    FeleceProject

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "assets/css/style.min.css": "assets/css/custom.less"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['assets/css/**/custom.less'],
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                src: 'assets/**',
+                dest: 'backup',
+            },
+        },
+        autoprefixer:{
+            dist:{
+                files:{
+                    'assets/css/style.min.css':'assets/css/style.min.css'
+                }
+            }
+        },
+        processhtml: {
+            build: {
+                files: {
+                    'index.html' : ['index.html']
+                }
+            }
+        },
+        browserSync: {
+            resource: {
+                    files: {
+                    src: ['assets/**/*.css', 'assets/**/*.js', '*.html']
+                },
+                    options: {
+                    watchTask: true,
+                    livereload: true,
+                    server: './'
+                }
+            }
+        },
+
+        
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer'); 
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    // grunt.loadNpmTasks('grunt-contrib-concat'); (Eklentiyi çalıştırırken sorun yaşadım)  
+    // grunt.loadNpmTasks('grunt-contrib-clean'); (Eklentiyi çalıştırırken sorun yaşadım)
+    grunt.registerTask('default', ['copy', 'less', 'browserSync', 'watch']);
+
+};
